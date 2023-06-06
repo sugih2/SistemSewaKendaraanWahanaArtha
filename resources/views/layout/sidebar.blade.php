0834d6feac31ea642d55ab8076f54de0604b3a85
iksanjs <iksanjaya83@gmail.com>
2023-06-06 10:36:43 +0700

    merubah side bar dan dashboard

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -30,13 +30,14 @@
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-    <i class="fas fa-fw fa-cog"></i>
-    <span>Components</span>
+    <i class="fas fa-sw fa-taxi"></i>
+    <span>Kendaraan</span>
   </a>
   <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Components:</h6>
-      <a class="collapse-item" href="#">Buttons</a>
+      <h6 class="collapse-header">Kelola Kendaraan</h6>
+      <a class="collapse-item" href="#">Tambah Kendaraan</a>
+      <a class="collapse-item" href="#">Data Kendaraan</a>
     </div>
   </div>
 </li>
@@ -44,14 +45,15 @@
 <!-- Nav Item - Utilities Collapse Menu -->
 <li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-    <i class="fas fa-fw fa-wrench"></i>
-    <span>Utilities</span>
+    <i class="fas fa-fw fa-id-card"></i>
+    <span>Surat Kendaraan</span>
   </a>
   <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Utilities:</h6>
-      <a class="collapse-item" href="#">Colors</a>
-      <a class="collapse-item" href="#">Borders</a>
+      <h6 class="collapse-header">Kelola Surat Kendaraan</h6>
+      <a class="collapse-item" href="#">Kelola Data BPKB</a>
+      <a class="collapse-item" href="#">Kelola Data STNK</a>
+      <a class="collapse-item" href="#">Kelola Data KIR</a>
     </div>
   </div>
 </li>
@@ -64,38 +66,34 @@
   Sewa Kendaraan
 </div>
 
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Kelola Service
+</div>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Kelola Pengguna
+</div>
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-    <i class="fas fa-fw fa-folder"></i>
-    <span>Pages</span>
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePengguna" aria-expanded="true" aria-controls="collapsePengguna">
+    <i class="fas fa-user fa-sm fa-fw"></i>
+    <span>User</span>
   </a>
-  <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+  <div id="collapsePengguna" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Login Screens:</h6>
-      <a class="collapse-item" href="#">Login</a>
-      <a class="collapse-item" href="#">Register</a>
-      <a class="collapse-item" href="#">Forgot Password</a>
-      <div class="collapse-divider"></div>
-      <h6 class="collapse-header">Other Pages:</h6>
-      <a class="collapse-item" href="#">404 Page</a>
-      <a class="collapse-item" href="#">Blank Page</a>
+      <h6 class="collapse-header">Kelola User:</h6>
+      <a class="collapse-item" href="{{ route('register') }}">Tambah User</a>
+      <a class="collapse-item" href="#">Data User</a>
     </div>
   </div>
-</li>
-
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-  <a class="nav-link" href="#">
-    <i class="fas fa-fw fa-chart-area"></i>
-    <span>Charts</span></a>
-</li>
-
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-  <a class="nav-link" href="#">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Tables</span></a>
 </li>
 
 <!-- Divider -->
